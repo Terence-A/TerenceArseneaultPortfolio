1d1e49ba3343ee73c9e80022887ef0aa8a7b51f2
terenceDev <terence.arseneault@hotmail.com>
2023-09-29 07:55:10 -0300

    refactored the Portfolio list to map through data

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -10,130 +10,80 @@ import weatherDashboard from "./weatherDashboard.png";
 import workDayScheduler from "./workDayScheduler.png";
 import codingQuiz from "./codingQuiz.png";
 
+const PortfolioList = [
+  {
+    portLink: "https://github.com/Terence-A/ByteSizeBlog",
+    portTitle: "Byte Size Code Repo",
+    image: byteSizeBlog,
+    deployLink: "https://byte-size-blog-29657a568883.herokuapp.com/",
+    deployTitle: "Deploy Byte Size Blog",
+    id: 1,
+  },
+  {
+    portLink: "https://github.com/Terence-A/Atlantic-Glamping",
+    portTitle: "Atlantic Glamping Repo",
+    image: atlanticGlamping,
+    deployLink: "https://atlantic-glamping-6972d1dfb7e3.herokuapp.com/",
+    deployTitle: "Deploy Atlantic Glamping",
+    id: 2,
+  },
+  {
+    portLink: "https://github.com/Terence-A/WhatsForDinner",
+    portTitle: "What's for Dinner Repo",
+    image: whatsForDinner,
+    deployLink: "https://terence-a.github.io/WhatsForDinner/",
+    deployTitle: "Deploy Whats for Dinner",
+    id: 3,
+  },
+  {
+    portLink: "https://github.com/Terence-A/WeatherDashboard",
+    portTitle: "Weather Dashboard Repo",
+    image: weatherDashboard,
+    deployLink: "https://terence-a.github.io/WeatherDashboard/",
+    deployTitle: "Deploy Weather Dashboard",
+    id: 4,
+  },
+  {
+    portLink: "https://github.com/Terence-A/workDayScheduler",
+    portTitle: "Work Day Scheduler Repo",
+    image: workDayScheduler,
+    deployLink: "https://terence-a.github.io/workDayScheduler/",
+    deployTitle: "Deploy Work Day Scheduler",
+    id: 5,
+  },
+  {
+    portLink: "https://github.com/Terence-A/codingQuizChallenge",
+    portTitle: "Coding Quiz Challenge Repo",
+    image: codingQuiz,
+    deployLink: "https://terence-a.github.io/codingQuizChallenge/",
+    deployTitle: "Deploy Coding Quiz Challenge",
+    id: 6,
+  },
+];
+
+console.log(PortfolioList[0].portLink);
+
 class Portfolio extends Component {
   render() {
     return (
       <div className="carouselWrapper">
         <h2>My Work</h2>
         <Carousel className="mainCarousel" showThumbs={false}>
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Terence-A/ByteSizeBlog"
-            >
-              <h3>- Byte Size Code Repo -</h3>
-              <img src={byteSizeBlog} alt="" className="imgOne image" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://byte-size-blog-29657a568883.herokuapp.com/"
-            >
-              <p className="legend">Deploy Byte Size Blog</p>
-            </a>
-          </div>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Terence-A/Atlantic-Glamping"
-            >
-              <h3>- Atlantic Glamping Repo-</h3>
-              <img src={atlanticGlamping} alt="" className="imgTwo image" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://atlantic-glamping-6972d1dfb7e3.herokuapp.com/"
-            >
-              <p className="legend">Deploy Atlantic Glamping</p>
-            </a>
-          </div>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Terence-A/WhatsForDinner"
-            >
-              <h3>- What's for Dinner Repo -</h3>
-              <img
-                src={whatsForDinner}
-                alt="clear"
-                className="imgThree image"
-              />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://terence-a.github.io/WhatsForDinner/"
-            >
-              <p className="legend"> Deploy Whats for Dinner </p>
-            </a>
-          </div>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Terence-A/WeatherDashboard"
-            >
-              <h3>- Weather Dashboard Repo -</h3>
-              <img
-                src={weatherDashboard}
-                alt="clear"
-                className="imgThree image"
-              />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://terence-a.github.io/WeatherDashboard/"
-            >
-              <p className="legend"> Deploy Weather Dashboard</p>
-            </a>
-          </div>
-
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Terence-A/workDayScheduler"
-            >
-              <h3>- Work Day Scheduler Repo -</h3>
-              <img
-                src={workDayScheduler}
-                alt="clear"
-                className="imgThree image"
-              />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://terence-a.github.io/workDayScheduler/"
-            >
-              <p className="legend">Deploy Work Day Scheduler</p>
-            </a>
-          </div>
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Terence-A/codingQuizChallenge"
-            >
-              <h3>- Coding Quiz Challenge Repo -</h3>
-              <img src={codingQuiz} alt="clear" className="imgThree image" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://terence-a.github.io/codingQuizChallenge/"
-            >
-              <p className="legend">Deploy Coding Quiz Challenge</p>
-            </a>
-          </div>
+          {PortfolioList.map((list) => (
+            <div key={list.id}>
+              <a target="_blank" rel="noopener noreferrer" href={list.portLink}>
+                <button>{list.portTitle}</button>
+                <img src={list.image} alt="" className="imgOne image" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={list.deployLink}
+              >
+                <p className="legend">{list.deployTitle}</p>
+              </a>
+            </div>
+          ))}
         </Carousel>
       </div>
     );
